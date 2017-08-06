70e8709e2e4f7ce29e4742aaec826328be0b3df6
leticiacostadias <leticia.costadias@gmail.com>
2017-08-06 07:36:08 -0300

    Adding styles

--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,28 @@ const create = $('.lobby__create'),
 	enter = $('.lobby__enter'),
 	detail = $('.lobby__detail');
 
-$('#cancelTable').click(() => {
-	console.log('save');
+$('#saveTable').click(() => {
 	create.removeClass('show');
 	create.addClass('hidden');
 	enter.removeClass('hidden');
 	enter.addClass('show');
 });
 
+$('#cancelTable').click(() => {
+	create.removeClass('show');
+	create.addClass('hidden');
+	enter.removeClass('hidden');
+	enter.addClass('show');
+});
+
+$('#voltar').click(() => {
+	detail.removeClass('show');
+	detail.addClass('hidden');
+	enter.removeClass('hidden');
+	enter.addClass('show');
+});
+
 $('.not-list').click(() => {
-	console.log('detail');
 	enter.removeClass('show');
 	enter.addClass('hidden');
 	detail.removeClass('hidden');
